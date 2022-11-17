84eb62be3edfb6498e19b63f4c8f4712095c4497
Olintho <olinthojesus@yahoo.com.br>
2022-11-17 17:56:14 -0300

    Database

--- backend/src/main/java/br/com/orei/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/br/com/orei/dsmeta/DsmetaApplication.java
@@ -3,6 +3,10 @@ package br.com.orei.dsmeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO : HEREIN -> OSJ Capitulo 2 -> 29:34 -> *** https://youtu.be/vjU2RGPp1cU?t=
+// https://youtu.be/vjU2RGPp1cU?t=
+// Estou seguindo enquanto ele explica o que já sei
+
 @SpringBootApplication
 public class DsmetaApplication {
 
