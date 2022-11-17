3f0f01a2dd2fe0a211919e4e8d166f186adcba96
Olintho <olinthojesus@yahoo.com.br>
2022-11-17 14:36:19 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,11 +7,10 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                    <h1>DSMeta</h1>
-                    <p>Desenvolvido por <a href="https://www.instagram.com/olinthoosj/"> @olinthoosj </a></p>
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="https://www.instagram.com/olinthoosj/"> @olinthoosj </a></p>
             </div>
         </header>
-
     )
 }
 
