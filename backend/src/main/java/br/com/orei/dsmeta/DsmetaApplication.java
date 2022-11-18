9f306878545b2dad765d6a538718caed697720de
Olintho <olinthojesus@yahoo.com.br>
2022-11-18 16:02:34 -0300

    Twilio SMS

--- backend/src/main/java/br/com/orei/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/br/com/orei/dsmeta/DsmetaApplication.java
@@ -3,7 +3,9 @@ package br.com.orei.dsmeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO : HEREIN -> OSJ Capitulo 2 -> 33:34 -> *** https://youtu.be/vjU2RGPp1cU?t=
+//TODO : HEREIN -> OSJ Capitulo 2 -> 1h:12:00 -> *** https://youtu.be/vjU2RGPp1cU?t=
+// Envio SMS -> Cadastrar-me no Twilio 
+// Add dependencies on Maven
 
 
 @SpringBootApplication
---
+++ backend/src/main/java/br/com/orei/dsmeta/services/SmsService.java
@@ -0,0 +1,52 @@
+package br.com.orei.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import br.com.orei.dsmeta.entities.Sale;
+import br.com.orei.dsmeta.repository.SaleRepository;
+
+@Service
+public class SmsService {
+	
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+
+		Sale sale = saleRepository.findById(saleId).get();
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msgSms = "O vendedor '" + sale.getSellerName() 
+						+ "', no período de " + date + " vendeu um total de R$ " 
+						+ String.format("%.2f", sale.getAmount()) + ".";   
+		
+		System.out.println(msgSms);
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msgSms).create();
+
+		System.out.println(message.getSid());
+	}
+}
