73ae15d6a8523c80a832611878d437545bb355ea
Olintho <olinthojesus@yahoo.com.br>
2022-11-18 09:20:06 -0300

    Date select

--- backend/src/main/java/br/com/orei/dsmeta/services/SaleService.java
+++ backend/src/main/java/br/com/orei/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package br.com.orei.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.orei.dsmeta.entities.Sale;
@@ -13,11 +17,23 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository saleRepository;
-	
-	public List<Sale> findSales() {
-		
-//		List<Sale> list = new List<Sale>(); 
-		return saleRepository.findAll();
-		
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+//		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		try {
+
+			LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+			LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+			return saleRepository.findSales(min, max, pageable);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+
 	}
 }
